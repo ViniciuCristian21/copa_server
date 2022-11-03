d3b595c4ba48983469ab690ef81d2b7a752aa090
Viniciu Cristian <git.viniciu@gmail.com>
2022-11-02 23:45:10 -0300

    aula 02.1

--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,18 @@ async function bootstrap() {
         return {count}
     })
 
+    fastify.get('/users/count', async () => {
+        const count = await prisma.user.count()
+
+        return {count}
+    })
+
+    fastify.get('/guesses/count', async () => {
+        const count = await prisma.guess.count()
+
+        return {count}
+    })
+
     fastify.post('/pools', async (request, reply) => {
         const createPoolBody = z.object({
             title: z.string(),
@@ -44,7 +56,7 @@ async function bootstrap() {
         return reply.status(201).send({code})
     })
 
-    await fastify.listen({port: 3333, host: '0.0.0.0'});
+    await fastify.listen({port: 3333, /*host: '0.0.0.0'*/});
 }
 
 bootstrap();
